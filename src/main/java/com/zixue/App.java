2d910cedfe5d53f3f16366d62c55006f0b663ae7
huahua_global <huahua@163.com>
2021-09-13 15:58:27 +0800

    首次提交

---
+++ src/main/java/com/zixue/App.java
@@ -0,0 +1,13 @@
+package com.zixue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/zixue/controller/HelloController.java
@@ -0,0 +1,10 @@
+package com.zixue.controller;
+
+/**
+ * @author 嘟嘟的掌柜
+ * @constellation 白羊座
+ */
+
+public class HelloController {
+
+}
